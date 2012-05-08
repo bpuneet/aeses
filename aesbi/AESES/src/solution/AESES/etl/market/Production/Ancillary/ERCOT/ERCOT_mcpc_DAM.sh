07c0d0ac06df34952bd85171c1577a1ca8244ea5
unknown <puneet@SISBLRLPT051.sigmainfo.com>
2012-05-08 17:58:48 +0530

    Signed-off-by: unknown <puneet@SISBLRLPT051.sigmainfo.com>

---
+++ aesbi/AESES/src/solution/AESES/etl/market/Production/Ancillary/ERCOT/ERCOT_mcpc_DAM.sh
@@ -0,0 +1,2 @@
+cd /usr/local/pentaho-bi-suite/pentaho/design-tools/data-integration
+./pan.sh -file=/var/www/html/cron/Production/Ancillary/ERCOT/Ercot_DAM_Capacity_prices.ktr >> /var/www/html/cron/Production/Ancillary/ERCOT/ERCOT_download.log
